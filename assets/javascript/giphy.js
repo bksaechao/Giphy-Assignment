03c56cc1f9b99103918be61447ac3dfe57f3f1a5
CaoKao <bksaechao@gmail.com>
2019-10-26 16:21:41 -0700

    used the api to refernce the rating and held it in a p tag within the jquery file. created a new div to hold the gif img and gif rating within the jquery file. floated the individual divs left to stack nicely next to each other

--- assets/javascript/giphy.js
+++ assets/javascript/giphy.js
@@ -1,5 +1,5 @@
 //Initial array of topics
-var topics = ["Coding", "Toonami", "Sleep", "Halloween", "Sushi", "Tetris", "Beer", "Music", "Eeyore"]
+var topics = ["Snow", "Toonami", "Beer", "Portland", "Space", "Disney", "Ramen", "Dog", "Eeyore"]
 
 //Button maker
 function displayBtn() {
@@ -24,7 +24,7 @@ function addBtn() {
 function displayGif(name) {
     //Linking the API
     var apiKey = "JH9N0IxrYLjd5odOGP5tthHGw4ZUxFUj"
-    var queryURL = 'https://api.giphy.com/v1/gifs/search?q=' + name + '&api_key=' + apiKey + '&limit=10'
+    var queryURL = 'https://api.giphy.com/v1/gifs/search?q=' + name + '&api_key=' + apiKey + '&rating=pg&limit=10'
     $.ajax({
         url: queryURL,
         method: 'GET'
@@ -32,6 +32,9 @@ function displayGif(name) {
         .then(function (response) {
             var results = response.data;
             for (var j = 0; j < results.length; j++) {
+                var gifDiv = $("<div>");
+                gifDiv.addClass("gDiv");
+                var gRating = $("<p class='rating'>").text("Rating: " + results[j].rating.toUpperCase());
                 var gifURL = results[j].images.original_still.url
                 var gif = $("<img>");
                 gif.addClass("gif");
@@ -41,12 +44,15 @@ function displayGif(name) {
                 gif.attr("data-still", gifURL);
                 gif.attr("data-animate", results[j].images.original.url)
                 gif.attr("data-status", "still")
-                $("#giphy-list").append(gif);
+                gifDiv.append(gRating);
+                gifDiv.append(gif);
+                $("#giphy-list").append(gifDiv);
                 console.log(response);
             }
         });
 }
 
+//Function to stop and start the gif
 function animate() {
     if ($(this).attr("data-status") === "still") {
         $(this).attr("src", $(this).attr("data-animate"));
