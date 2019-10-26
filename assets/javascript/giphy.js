e4aac262274ec48ada6ff175203315d930bfd0f9
CaoKao <bksaechao@gmail.com>
2019-10-25 17:08:39 -0700

    linked the api in the javascript file

--- assets/javascript/giphy.js
+++ assets/javascript/giphy.js
@@ -1,9 +1,19 @@
-var topics = ["Anime", "Basketball", "Music", "Food", "Sushi"]
+var topics = ["Anime", "Lucky Charms", "Music", "Halloween", "Sushi", "Video Games", "Heroes"]
 
 $(document).ready(function () {
     for (i = 0; i < topics.length; i++) {
         var btn = $("<button>");
         btn.text(topics[i]);
-        $("#button-list").prepend(btn);
+        $("#button-list").append(btn);
+        console.log(topics[i])
     }
+
+    //API key
+    var apiKey = "JH9N0IxrYLjd5odOGP5tthHGw4ZUxFUj"
+    var queryURL = 'https://api.giphy.com/v1/gifs/search?q=' + topics + '&api_key=JH9N0IxrYLjd5odOGP5tthHGw4ZUxFUj&limit=10'
+    $.ajax({
+        url: queryURL,
+        method: 'GET'
+    })
+        .then(function (response) { });
 });
